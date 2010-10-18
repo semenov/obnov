c8b151197600d254799219eba7982d9016aa2882
Vlad Semenov <vladislav.semenov@gmail.com>
2010-10-18 07:35:17 +0000

    Initial Rails 3 project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Obnov::Application.config.secret_token = '37561605ff2fa04b734c53beb53ac5fc6279ca4849c0f90d0eae8781be682870645aa5102ecbe0931ad1c690a6d95a79081493af81565e3ebb83a3d2a5b75ce6'
