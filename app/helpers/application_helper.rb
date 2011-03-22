c334d6c9085c9d2ee07f7838494494ad828601a5
Vladislav Semenov <vladislav.semenov@gmail.com>
2011-03-22 09:27:05 +0600

    Prettified posts & comments representation

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,8 +1,8 @@
 module ApplicationHelper
   def gravatar_for user, options = {}
-    options = {:alt => user.email, :size => 30}.merge! options
+    options = {:alt => user.email, :size => 40}.merge! options
     id = Digest::MD5::hexdigest user.email.strip.downcase
-    url = 'http://www.gravatar.com/avatar/' + id + '.jpg?d=retro&s=' + options[:size].to_s
+    url = 'http://www.gravatar.com/avatar/' + id + '.jpg?d=identicon&s=' + options[:size].to_s
     options.delete :size
     image_tag url, options
   end
